a742c241d5058762717e25818540c1089d8503c4
taylormaurer4323 <taylormaurer4323@gmail.com>
2023-04-23 15:30:40 -0600

    working version, now going to cleanup

--- icp_map_building/icp_tester.py
+++ icp_map_building/icp_tester.py
@@ -19,7 +19,7 @@ import math
 YEAR = 2022
 MONTH = 3
 
-data_dir = '/home/taylor/SJSU-ANS_DeepRacer/data/good_collect'
+data_dir = '/home/taylor/SJSU-ANS_DeepRacer/data/slow_steady_clunky'
 files = os.listdir(data_dir)
 times = np.array([])
 subfiles = np.array([])
@@ -43,7 +43,7 @@ sorted_files = subfiles[sorted_is]
 #Shorten because need to skip intor
 sorted_files = sorted_files[70:-1]
 #Identify reference frame
-start_index = 0
+start_index = 300
 for f in sorted_files:
     if 'nparray' in f:
         ref_id = start_index
@@ -67,6 +67,7 @@ sorted_times = sorted_times[ref_id + 1:-1]
 
 sorted_files = sorted_files[ref_id + 1:-1]
 pc_map = clean_ref_3dof[0:2, :].transpose()
+full_pc_map = []
 Rest_obs = np.identity(2)
 test_obs = np.zeros((2,1))
 translation = []
@@ -103,7 +104,7 @@ for f in sorted_files:
         #Attempt transform backt o map frame:
         est_pc_map = np.matmul(Rest_obs, obs_2dof.transpose()) + test_obs                    
         (Rmap_est, tmap_est, dont_use, outlier_mask) = icp.run_icp(pc_map, est_pc_map.transpose(), .1, 
-                                                                   distance_threshold=.1, verbose=False)        
+                                                                   distance_threshold=.5, verbose=False)        
 
         #Print the rotation angle and translation        
         rot_angle = math.acos(Rmap_est[0, 0])
@@ -137,16 +138,23 @@ for f in sorted_files:
         #Only add to the map if the outlier count is below 10        
         pc_map = np.vstack((pc_map, tmp_pc_map[outlier_mask, :]))
 
-        if c > 330:
+        if c > 1200:
             break
         #plt.scatter(pc_map[:,0], pc_map[:,1], color='blue')
         #plt.scatter(tmp_pc_map[np.invert(outlier_mask), 0], tmp_pc_map[np.invert(outlier_mask), 1], color='red')
         #plt.show()
 
     #if c > 20 and c % 10 == 0:
-    #if c % 100 == 0 and c > 1:
+    if c % 100 == 0 and c > 1:
     #    plt.scatter(pc_map[:,0], pc_map[:,1], color='blue')
-    #    pc_map = icp.trimPointsV2(pc_map, scan_radius = .1, POINT_THRESH=1)
+        pc_map = icp.trimPointsV2(pc_map, scan_radius = .1, POINT_THRESH=25)
+        #Add the pc map to the full one:
+        if full_pc_map == []:
+            full_pc_map = np.copy(pc_map)
+        else:
+            full_pc_map = np.vstack((full_pc_map, pc_map))
+        pc_map = np.copy(full_pc_map)
+
     #    plt.scatter(pc_map[:,0], pc_map[:,1], color='green')
     #    plt.show()
 
@@ -154,6 +162,8 @@ for f in sorted_files:
         #pc_map = icp.trimPoints(pc_map, include_radius=.05)
     if c % 20 == 0 and c > 1:
         plt.scatter(pc_map[:,0], pc_map[:,1], color='blue')
+        if not full_pc_map == []:
+            plt.scatter(full_pc_map[:,0], full_pc_map[:,1], color='green')
         tmp_sensor = np.array(sensor_loc)
         tmp_sensor = tmp_sensor.reshape((tmp_sensor.shape[0], tmp_sensor.shape[1]))
         plt.scatter(tmp_sensor[:,0], tmp_sensor[:,1], color='magenta')
@@ -165,18 +175,18 @@ for f in sorted_files:
 plt.scatter(pc_map[:,0], pc_map[:,1], color='blue')
 #plt.scatter(tmp_pc_map[np.invert(outlier_mask), 0], tmp_pc_map[np.invert(outlier_mask), 1], color='red')
 
-redux_pc_map = icp.trimPointsV2(pc_map, scan_radius = .1, POINT_THRESH=20)
-plt.scatter(redux_pc_map[:,0], redux_pc_map[:,1], color='green')
+#redux_pc_map = icp.trimPointsV2(pc_map, scan_radius = .1, POINT_THRESH=20)
+#plt.scatter(redux_pc_map[:,0], redux_pc_map[:,1], color='green')
 plt.show()
 # %%
 #Reepat
 pc_map = redux_pc_map
-c = 0
-#shorted_sorted_files = sorted_files[c:-1]
-Rest_obs = np.identity(2)
-test_obs = np.zeros((2,1))
+#c = 0
+shorted_sorted_files = sorted_files[c:-1]
+#Rest_obs = np.identity(2)
+#test_obs = np.zeros((2,1))
 #%%
-for f in sorted_files:    
+for f in shorted_sorted_files:    
     if 'nparray' in f and c % 1 == 0:
         time.append(sorted_times[c])
         with open(os.path.join(data_dir, f), 'rb') as handle:
@@ -193,10 +203,10 @@ for f in sorted_files:
         #Run ICP to get pose info:
         #Expects size Nx2
         #Attempt transform backt o map frame:
-        #est_pc_map = np.matmul(Rest_obs, obs_2dof.transpose()) + test_obs                    
-        est_pc_map = obs_2dof.transpose()
+        est_pc_map = np.matmul(Rest_obs, obs_2dof.transpose()) + test_obs                    
+        
         (Rmap_est, tmap_est, dont_use, outlier_mask) = icp.run_icp(pc_map, est_pc_map.transpose(), .1, 
-                                                                   distance_threshold=.1, verbose=False)        
+                                                                   distance_threshold=.5, verbose=False)        
         #Print the rotation angle and translation        
         rot_angle = math.acos(Rmap_est[0, 0])
         rot_angle_check = math.asin(Rmap_est[1,0])
@@ -226,7 +236,7 @@ for f in sorted_files:
         #Only add to the map if the outlier count is below 10        
         pc_map = np.vstack((pc_map, tmp_pc_map[outlier_mask, :]))
 
-        if c > 330:
+        if c > 1100:
             break
 
     if c % 20 == 0 and c > 1:
@@ -242,7 +252,7 @@ for f in sorted_files:
 plt.scatter(pc_map[:,0], pc_map[:,1], color='blue')
 #plt.scatter(tmp_pc_map[np.invert(outlier_mask), 0], tmp_pc_map[np.invert(outlier_mask), 1], color='red')
 
-redux_pc_map = icp.trimPointsV2(pc_map, scan_radius = .1, POINT_THRESH=20)
+redux_pc_map = icp.trimPointsV2(pc_map, scan_radius = .1, POINT_THRESH=10)
 plt.scatter(redux_pc_map[:,0], redux_pc_map[:,1], color='green')
 plt.show()
 
