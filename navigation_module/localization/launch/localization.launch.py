dd64b8e40cf6b2c3b5a2d487ceaa7479aa045300
taylormaurer4323 <taylormaurer4323@gmail.com>
2023-04-23 21:05:55 -0400

    updated the launch file to launch all things needed for localization and added flag to start icp once map received

--- navigation_module/localization/launch/localization.launch.py
+++ navigation_module/localization/launch/localization.launch.py
@@ -8,12 +8,32 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-     localization = Node(
-        package='localization',
-        executable='localization',
-        output='screen'
-    ) 
-    
      return LaunchDescription([
-        localization
+        Node(
+            package='localization',
+            executable='localization',
+            output='screen'
+        ),
+        Node(
+            package='rplidar_ros2',
+            executable='rplidar_scan_publisher',
+            name='rplidar_scan_publisher',
+            namespace='rplidar_ros',
+            parameters=[{
+                'serial_port': '/dev/ttyUSB0',
+                'serial_baudrate': 115200,
+                'frame_id': 'laser',
+                'inverted': False,
+                'angle_compensate': True
+            }]
+        ),
+        Node(
+            package='lidar_scan_acq',
+            executable='lidar_scan_acq',
+            output='screen',
+            parameters=[
+                {'saveData': False,
+                 'maxRange': 12.0}
+            ]
+        )
     ])
