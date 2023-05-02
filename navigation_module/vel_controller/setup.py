c8805f1f3a7b057237aebfd32033ce1ddf5022c9
taylormaurer4323 <taylormaurer4323@gmail.com>
2023-05-02 01:38:42 -0400

    hopefully fixed the launch file issue

--- navigation_module/vel_controller/setup.py
+++ navigation_module/vel_controller/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from glob import glob
+import os
 package_name = 'vel_controller'
 
 setup(
@@ -8,10 +9,7 @@ setup(
     packages=[package_name, 'PID'],
     package_dir = {'PID': '.'},
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (glob('launch/*.launch.py'))
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
