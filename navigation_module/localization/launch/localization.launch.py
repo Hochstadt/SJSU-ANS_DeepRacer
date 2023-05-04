362de65f9b9510fed16f9695a32474c09d33acf3
curtisjm17 <curtis.miller@sjsu.edu>
2023-05-03 20:34:15 -0600

    Updates to Localization to use python and open3d icp implementation

--- navigation_module/localization/launch/localization.launch.py
+++ navigation_module/localization/launch/localization.launch.py
@@ -1,36 +1,29 @@
-import launch
-import launch.actions
-import launch.substitutions
-import launch_ros.actions
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
-
-     localization = Node(
-        package='localization',
-        executable='localization',
-        output='screen',
-        parameters=[
-                {'icp_fit_thresh': 0.025,
-                 'gicp_fit_thresh': 0.025,
-                 'icp_trans_eps': 1e-8,
-                 'icp_max_corr_dist': 1000.0,
+    return LaunchDescription([
+        Node(
+        	package='localization',
+        	executable='localization',
+        	output='screen',
+            parameters=[
+                {'downsampleMap': True,
+                 'downsampleValue': 0.25,
+                 'coarse_fit_thresh': 0.25,
+                 'fine_fit_thresh': 0.25,
+                 'icp_max_corr_dist': 1.0,
                  'icp_max_iters': 100,
-                 'min_search_x': -12.0,
-                 'max_search_x':  12.0,
+                 'min_search_x': -10.0,
+                 'max_search_x':  2.0,
                  'delta_search_x': 5,
-                 'min_search_y': -12.0,
-                 'max_search_y':  12.0,
+                 'min_search_y': -2.0,
+                 'max_search_y':  1.0,
                  'delta_search_y': 5,
                  'delta_search_ang': 5,
                  'rotationCheckThreshold': 0.017,
-                 'positionCheckThrehsold': 0.010
+                 'positionCheckThrehsold': 0.100,
                  }
             ]
-    ) 
-    
-     return LaunchDescription([
-        localization
+        )
     ])
